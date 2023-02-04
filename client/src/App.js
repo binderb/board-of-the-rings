37f44ddeb9742208745ed6e95cfd7e154722f858
Ben Binder <ben.binder11@gmail.com>
2023-02-04 11:27:40 -0600

    Implement basic socket connection.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import io from 'socket.io-client';
+
+// This connects our front-end user to the socket
+// server, and will prompt a message on the server.
+const socket = io.connect("http://localhost:3001");
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App online!<br/>
+      Check the server console to see when users connect to the app!
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,12 +1,45 @@
+const path = require('path');
 const express = require('express');
 const { Server } = require('socket.io');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Need to use CORS middleware during development
+// so that our socket server (on port 3001) can communicate
+// with our front-end (on port 3000) without throwing an error.
+// This is not necessary in production, since the front-end
+// will be served by Express on the same port as the back-end.
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+} else {
+  app.use(cors());
+}
 
 
 // This time, we actually need to use the HTTP server instance that is
 // returned by 'app.listen', so we capture it in a 'server' variable.
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}! 🧩`)
+});
+
+// Again, need to set up CORS for a development environment.
+const io = (process.env.NODE_ENV === 'production') 
+? 
+new Server(server)
+:
+new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+// Listen for socket events.
+io.on('connection', (socket) => {
+  console.log(`User Connected: ${socket.id}`);
 });
