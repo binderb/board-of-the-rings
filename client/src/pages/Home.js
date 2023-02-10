419618544871c928c2c6bf4f40179bca4b6a69c2
Ben Binder <ben.binder11@gmail.com>
2023-02-10 09:09:39 -0600

    Add start scripts for Heroku.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,14 +4,15 @@ export default function Home () {
 
   return (
     <>
-    <h1>Project 3 (Awesome Name TBD)</h1>
+    <h1>Board of the Rings</h1>
     <p>
       This is a basic landing page for our project. The links below provide access to different pages that we can work on separately or in groups.
     </p>
     <Link to='/signup'>Signup Page</Link><br/>
     <Link to='/login'>Login Page</Link><br/>
     <Link to='/profile'>Profile Page</Link><br/>
-    <Link to='/socket-test'>Socket Test Page</Link>
+    <Link to='/socket-test'>Socket Test Page</Link><br/>
+    <Link to='/game-session'>Game Session</Link>
     </>
   );
 }
