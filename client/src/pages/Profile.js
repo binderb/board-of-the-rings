a5ba1b482f02a98163e56e9906dcd4882e76c23f
Lily Russ <llyruss@yahoo.com>
2023-02-16 22:36:32 -0600

    profile page first iteration

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,10 +3,65 @@
 // Maybe the option to host/join a game could be here,
 // as well as win/loss records, etc.
 
-export default function Profile () {
+import React from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+
+import { useQuery } from '@apollo/client';
+import { QUERY_SINGLE_PROFILE, QUERY_ME } from '../utils/queries';
+
+import Auth from '../utils/auth';
+
+const Profile = (props) => {
+  const { username: userParam } = useParams();
+
+  const { loading, data } = useQuery(userParam ? QUERY_SINGLE_PROFILE : QUERY_ME, {
+    variables: { username: userParam },
+  });
+
+
+  const user = data?.me || data?.user || {};
+
+
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Navigate to="/profile" />;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this. Use the navigation links above to
+        sign up or log in!
+      </h4>
+    );
+  }
+
+  const handleClick = async () => {
+    try {
+      await logout({
+        variables: { id: user._id },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <>
-    <h1>Profile</h1>
+    <div>
+        <h2>
+          Welcome {userParam ? `${user.username}`: "Player"}.
+        </h2>
+          <button onClick={handleClick}>
+            Logout
+          </button>
+    </div>
     </>
   );
-}
+
+};
+
+export default Profile;
