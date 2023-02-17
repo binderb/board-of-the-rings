05186913aa6315ca3e78945b371383679f849a5f
rishabh.kamath <rshbhk22@gmail.com>
2023-02-16 19:03:15 -0600

    added queries.js and mutations.js to front end

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 // import { Link } from 'react-router-dom';
 
 // import { useMutation } from '@apollo/client';
-// import { ADD_PROFILE } from '../utils/mutations';
+import { ADD_PROFILE } from '../utils/mutations';
 
 import AuthService from '../utils/auth';
 
---
+++ client/src/utils/mutation.js
@@ -0,0 +1,25 @@
+import { gql } from '@apollo/client';
+
+export const ADD_PROFILE = gql`
+  mutation addProfile($name: String!, $email: String!, $password: String!) {
+    addProfile(name: $name, email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
+`;
