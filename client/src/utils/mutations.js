3c7593363dc722a22b547457b2ff99ff31e3a544
rishabh.kamath <rshbhk22@gmail.com>
2023-02-16 22:32:16 -0600

    modified queries and mutations for users in client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,12 +1,12 @@
 import { gql } from '@apollo/client';
 
-export const ADD_PROFILE = gql`
-  mutation addProfile($name: String!, $email: String!, $password: String!) {
-    addProfile(name: $name, email: $email, password: $password) {
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
       token
-      profile {
+      user {
         _id
-        name
+        username
       }
     }
   }
@@ -16,9 +16,9 @@ export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
-      profile {
+      user {
         _id
-        name
+        username
       }
     }
   }
