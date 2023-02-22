832d403cd75e3bf0e00f4fc728409204fd30b29f
rishabh.kamath <rshbhk22@gmail.com>
2023-02-22 00:52:29 -0600

    updated home page, cleaned up UI on signup and login pages

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,13 +3,16 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-      <ul style={{ display: 'flex', listStyle: 'none', margin: 0, padding: 0 }}>
-        <li style={{ marginRight: '1rem' }} className="rounded bg-green-800 p-1 px-2 hover:bg-green-700"><Link to="/">Home</Link></li>
-        <li style={{ marginRight: '1rem' }} className="rounded bg-green-800 p-1 px-2 hover:bg-green-700"><Link to="/Rules">Rules</Link></li>
+    <nav>
+      <div className='navtitle'>
+      <p >Board of The Rings</p></div>
+      <ul>
+        <li className="navlink"><Link to="/">Home</Link></li>
+        <li className="navlink"><Link to="/Rules">Rules</Link></li>
       </ul>
     </nav>
   );
 }
 
 export default Navbar;
+
