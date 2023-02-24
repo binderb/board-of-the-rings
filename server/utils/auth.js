b4cb8c8f6c350bc8cf44ebe0b02435513a06d4b7
Ben Binder <ben.binder11@gmail.com>
2023-02-24 01:24:10 -0600

    Change and obfuscate JWT secret.

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const secret = 'mysecretsshhhhh';
+const secret = process.env.JWT_SECRET;
 const expiration = '2h';
 
 module.exports = {
